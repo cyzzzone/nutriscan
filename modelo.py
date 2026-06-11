8df4a1d4dac9a0bb2cbcd1f596e93ca5705265df
cyzzzone <jenyffersouza611@gmail.com>
2026-06-11 10:11:37 -0300

    Delete modelo.py

--- modelo.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# Coloque sua chave aqui para testar
-genai.configure(api_key="AIzaSyCGRpk5LoL3ZOVHyFjXcqTdnHwLhNUSAL4")
-
-print("Modelos disponíveis para sua chave:")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(f"Nome: {m.name} | Descrição: {m.description}")
